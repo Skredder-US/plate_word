00500da9112f5d2aa08f5f2bd2b3313b634c1aef
Skredder <taylor_juve@outlook.com>
2023-06-25 13:22:13 -0700

    Buttons and info dialog

--- lib/main.dart
+++ lib/main.dart
@@ -36,14 +36,16 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (BuildContext context, BoxConstraints constraints) {
           var width = constraints.constrainWidth();
           var height = constraints.constrainHeight();
-          // img cannot resize larger than original (1200px)
-          var imgWidth = min((width / 1.2).round(), 1200);
+          var imgWidth = min((width / 1.2).round(), 600);
           var imgHeight = (imgWidth / 2).round();
           var imgTop = height / 12;
-          var imgLeft = width / 12;
-          var pad = 120;
+          var imgLeft = width / 2 - imgWidth / 2;
+          var pad = 60;
           var textLeft = min(imgLeft * 2, imgLeft + pad);
           var inputPadding = min(textLeft / 2, pad).toDouble();
+          var color = const Color.fromRGBO(12, 19, 74, 1);
+          var buttonTop = height / 1.2;
+          var buttonLeft = width / 2;
 
           return Stack(
             children: <Widget>[
@@ -59,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 left: textLeft,
                 child: DefaultTextStyle(
                   style: TextStyle(
-                      color: const Color.fromRGBO(12, 19, 74, 1),
+                      color: color,
                       fontSize: imgWidth / 4,
                       fontFamily: 'LicensePlate'),
                   child: const Text('ABC1234'),
@@ -68,10 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
               Positioned(
                 top: imgTop + imgHeight + 25,
                 left: imgLeft,
-                width: min(width / 1.2, 1200),
+                width: imgWidth.toDouble(),
                 child: TextField(
                   style: TextStyle(
-                      color: const Color.fromRGBO(12, 19, 74, 1),
+                      color: color,
                       fontSize: imgWidth / 4,
                       fontFamily: 'LicensePlate'),
                   decoration: InputDecoration(
@@ -81,6 +83,56 @@ class _MyHomePageState extends State<MyHomePage> {
                         top: inputPadding,
                         bottom: inputPadding - 15),
                     hintText: 'ABC',
+                  ),
+                ),
+              ),
+              Positioned(
+                  top: buttonTop,
+                  left: buttonLeft - 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Submit'),
+                  )),
+              Positioned(
+                top: buttonTop - 4,
+                left: buttonLeft - 100,
+                child: IconButton(
+                  icon: const Icon(Icons.report),
+                  tooltip: 'Impossible!',
+                  onPressed: () {},
+                ),
+              ),
+              Positioned(
+                top: buttonTop - 4,
+                left: buttonLeft + 50,
+                child: IconButton(
+                  icon: const Icon(Icons.info),
+                  tooltip: 'Info',
+                  onPressed: () => showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => Dialog(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                                'Enter a word that contains the letters found '
+                                'on the plate in the order found on the plate. '
+                                'Example: "ABC1234" can be "Abacus" because '
+                                'Abacus has A before B before C.'),
+                            const SizedBox(height: 15),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Close'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
